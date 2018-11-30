b75b24b7b45fd685627867bec824acfd362fdaad
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-30 09:36:55 +0700

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,15 +1,10 @@
 section .rodata
 	a11 db "Siapa nama Anda? "
-	l_a11 equ $ - a11
-	
+	l_a11 equ $ - a11	
 	a12 db "Halo, "
 	l_a12 equ $ - a12
-	
 	a13 db "!",10
 	l_a13 equ $ - a13
-
-	filename db "result"
-
 section .bss
 	z11 resb 30
 section .data
@@ -17,51 +12,38 @@ section .data
 
 _start:
 	call print_a11
-	call getName
+	call get_name
 	mov rax,-1
-	call cleanName
+	call clean_name
 	mov r15,rax
-	call writeFile
 	call print_a12
-	call printName
+	call print_name
 	call print_a13
 	mov rax,60
 	mov rdi,0
 	syscall
-writeFile:
-	mov rax,2
-	mov rdi,filename
-	mov rsi,1
-	mov rdx,439
-	syscall
-	mov rdi,rax
-	mov rax,1
-	mov rsi,a12
-	mov rdx,l_a12
-	syscall
-	ret
-cleanName:
+clean_name:
 	inc rax
 	mov rdi,255
 	mov rdx,[z11+rax]
 	and rdx,rdi
 	cmp rdx,10
-	je trimName
+	je trim_name
 	cmp rdx,0
-	jne cleanName
+	jne clean_name
 	ret
-trimName:
+trim_name:
 	mov r8,0
 	mov [z11+rax],r8
 	ret
-getName:
+get_name:
 	mov rax,0
 	mov rdi,0
 	mov rsi,z11
 	mov rdx,30
 	syscall
 	ret
-printName:
+print_name:
 	mov rax,1
 	mov rdi,1
 	mov rsi,z11
